7d876decb900ec8159208cc7f4be4d9f50881d01
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-07-01 15:55:46 +0200

    update

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -26,28 +26,3 @@ Filter.propTypes = {
 
 export default Filter;
 
-// class Filter extends Component {
-//     static propTypes = {
-//     value: PropTypes.string,
-//     onChange: PropTypes.func.isRequired
-//     };
-
-//     render() {
-//         const { value, onChange } = this.props;
-
-//         return (
-//             <div className={css["contact-forma"]}>
-//                 <h3 className={css.title}>Find contacts by name</h3>
-//                 <label>
-//                     <input className={css.filter}
-//                         type="text"
-//                         name="filter"
-//                         value={value}
-//                         onChange={onChange}
-//                     />
-//                 </label>
-//             </div>
-//         );
-//     }
-// }
-
