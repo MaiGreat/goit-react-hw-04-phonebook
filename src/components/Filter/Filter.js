510a8e42fdf9746f859f2075930f037eaec3bc65
Olha Maistrenko <olga.maistrenko@outlook.com>
2023-07-01 15:45:13 +0200

    update

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,32 +1,53 @@
-import PropTypes from "prop-types";
-import React, { Component } from 'react';
-import css from "./Filter.module.css"
+import React from 'react';
+import PropTypes from 'prop-types';
+import css from './Filter.module.css';
 
+const Filter = ({ value, onChange }) => {
+    return (
+        <div className={css['contact-forma']}>
+            <h3 className={css.title}>Find contacts by name</h3>
+            <label>
+                <input
+                    className={css.filter}
+                    type="text"
+                    name="filter"
+                    value={value}
+                    onChange={onChange}
+                />
+            </label>
+        </div>
+    );
+};
 
-
-class Filter extends Component {
-    static propTypes = {
+Filter.propTypes = {
     value: PropTypes.string,
     onChange: PropTypes.func.isRequired
-    };
-
-    render() {
-        const { value, onChange } = this.props;
-
-        return (
-            <div className={css["contact-forma"]}>
-                <h3 className={css.title}>Find contacts by name</h3>
-                <label>
-                    <input className={css.filter}
-                        type="text"
-                        name="filter"
-                        value={value}
-                        onChange={onChange}
-                    />
-                </label>
-            </div>
-        );
-    }
-}
+};
 
 export default Filter;
+
+// class Filter extends Component {
+//     static propTypes = {
+//     value: PropTypes.string,
+//     onChange: PropTypes.func.isRequired
+//     };
+
+//     render() {
+//         const { value, onChange } = this.props;
+
+//         return (
+//             <div className={css["contact-forma"]}>
+//                 <h3 className={css.title}>Find contacts by name</h3>
+//                 <label>
+//                     <input className={css.filter}
+//                         type="text"
+//                         name="filter"
+//                         value={value}
+//                         onChange={onChange}
+//                     />
+//                 </label>
+//             </div>
+//         );
+//     }
+// }
+
